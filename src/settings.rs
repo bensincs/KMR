1e6f208bbafdb150430f3d3ac88e092a04cadde8
Ben Sinclair <bensiclair@microsoft.com>
2023-10-24 20:26:14 +0100

    Random comments

--- src/settings.rs
+++ src/settings.rs
@@ -40,21 +40,12 @@ impl Settings {
         let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
 
         let s = Config::builder()
-            // Start off by merging in the "default" configuration file
             .add_source(File::with_name("config/default"))
-            // Add in the current environment file
-            // Default to 'development' env
-            // Note that this file is _optional_
             .add_source(File::with_name(&format!("config/{}", run_mode)).required(false))
-            // Add in a local configuration file
-            // This file shouldn't be checked in to git
             .add_source(File::with_name("config/local").required(false))
-            // Add in settings from the environment (with a prefix of APP)
-            // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
             .add_source(Environment::with_prefix("kmr").separator("__"))
             .build()?;
 
-        // You can deserialize (and thus freeze) the entire configuration as
         s.try_deserialize()
     }
 }
